c62704e54040045a82996a1a8ed3c67ae18d9088
douglas <douglas.arau@hotmail.com>
2024-03-13 19:02:30 -0300

    pasta repository feita

--- src/main/java/br/com/fiap/springpfauthentication/repository/SistemaRepository.java
+++ src/main/java/br/com/fiap/springpfauthentication/repository/SistemaRepository.java
@@ -3,6 +3,8 @@ package br.com.fiap.springpfauthentication.repository;
 import br.com.fiap.springpfauthentication.entity.Perfil;
 import br.com.fiap.springpfauthentication.entity.Sistema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SistemaRepository extends JpaRepository<Sistema, Long> {
 }
