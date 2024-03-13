bfeaf7bed4f98ed8e8624cb91631d94d1f69fb3d
douglas <douglas.arau@hotmail.com>
2024-03-13 19:00:50 -0300

    pessoa e sistema finalizado

--- src/main/java/br/com/fiap/springpfauthentication/entity/Sistema.java
+++ src/main/java/br/com/fiap/springpfauthentication/entity/Sistema.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 @Table(name = "TB_2TDSPF_SISTEMA")
 @Data
 @AllArgsConstructor
@@ -25,8 +28,37 @@ public class Sistema {
     )
     @Column(name = "ID_SISTEMA")
     private Long id;
+
     @Column(name = "NM_SISTEMA")
     private String nome;
+
     @Column(name = "SG_SISTEMA")
     private String sigla;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinTable(
+            name = "TB_2TDSPF_PERMISSOES",
+            joinColumns = {
+                    @JoinColumn(
+                            name = "SISTEMA",
+                            referencedColumnName = "ID_SISTEMA",
+                            foreignKey = @ForeignKey(
+                                    name = "FK_RESPONSAVEIS_SISTEMA"
+                            )
+                    )
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(
+                            name = "USUARIO",
+                            referencedColumnName = "ID_USUARIO",
+                            foreignKey = @ForeignKey(
+                                    name = "FK_SISTEMA_RESPONSAVEIS"
+                            )
+                    )
+            }
+    )
+
+    private Set<Usuario> responsaveis = new LinkedHashSet<>();
+
+
 }
